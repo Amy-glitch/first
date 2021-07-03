24b6183a9b4bfa429b4714b4ce1d18441e285ac3
Karen <ccheetah3012@gmail.com>
2021-07-02 18:11:19 -0700

    k

--- nodescript.js
+++ nodescript.js
@@ -1,22 +1,23 @@
 var http = require('http');
-var dt = require('./mymodule');
-var url = require('url');
-var fs = require('fs');
+// var dt = require('./mymodule');
+// var url = require('url');
+// var fs = require('fs');
 
 
 http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'}); //http header
-// //   res.write(req.url); //get request url
+  res.writeHead(200, {'Content-Type': 'text/html'}); //http header
+  res.write("heyo");
+//   res.write(req.url); //get request url
 //   var q = url.parse(req.url, true).query;
 //   var txt = q.year + " " + q.month;
 //   res.write(txt);
 //   res.end('Hello World!'+dt.myDateTime());
 
-fs.readFile('mytextfile.txt', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+// fs.readFile('mytextfile.txt', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     return res.end();
+//   });
 
 
 
