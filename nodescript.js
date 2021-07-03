2385b809f21df6d28af5ce1174cc64588a329e9a
Karen <ccheetah3012@gmail.com>
2021-07-03 11:38:19 -0700

    m

--- nodescript.js
+++ nodescript.js
@@ -33,6 +33,8 @@ client.query('SELECT * FROM users;', (err, ress) => {
   }
  // iu.insertUser(client);
  client.query('INSERT INTO users(u_id,username,pwd) VALUES(2,"pofpof","pof1");', (err, ress) => {
+   console.log(err);
+  console.log(ress);
   client.end();
 
 });
