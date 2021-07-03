c10efeb7e1e2e34895e1e5ce58c4f663c77c926e
Karen <ccheetah3012@gmail.com>
2021-07-03 07:46:29 -0700

    ddd

--- nodescript.js
+++ nodescript.js
@@ -21,12 +21,13 @@ const client = new Client({
   }
 });
 client.connect();
-client.query('SELECT * FROM users;', (err, res) => {
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();}
+  client.end();
+});
 
 
 
