52544457c2147de5ae80194ec9ab1fd3a1e58ecc
Karen <ccheetah3012@gmail.com>
2021-07-03 11:27:50 -0700

    m

--- insertuser.js
+++ insertuser.js
@@ -1,8 +1,11 @@
-exports.insertUser = function () {
+exports.insertUser = function (client) {
     
     
     
-    
+    client.query('INSERT INTO users(username,pwd) VALUES("pofpof","pof1");', (err, ress) => {
+        console.log(ress);
+
+    });
     
     
     
